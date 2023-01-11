41f353e90d663028cc879d46a83193b30fce1a83
guoping <git@easyandroid.cc>
2023-01-11 17:50:28 +0800

    fix anchor 底部坐标位置，这里他剪掉了通知栏高度，使用下面要把通知栏高度加上，不然底部会显示一个空白 版本小于Android 10的不需要，不然会高度太高

--- easydropdownmenu/src/main/java/cc/easyandroid/menu/core/AnimatorPopup.java
+++ easydropdownmenu/src/main/java/cc/easyandroid/menu/core/AnimatorPopup.java
@@ -3,6 +3,7 @@ package cc.easyandroid.menu.core;
 import android.content.res.Resources;
 import android.graphics.Rect;
 import android.os.Build;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.PopupWindow;
 
@@ -14,13 +15,15 @@ public class AnimatorPopup extends PopupWindow {
     }
 
     public void showAsDropDown(View anchor, int xoff, int yoff) {
-        if (Build.VERSION.SDK_INT >= 24) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             Rect visibleFrame = new Rect();
             anchor.getGlobalVisibleRect(visibleFrame);
             Resources resources = anchor.getResources();
             int height = resources.getDisplayMetrics().heightPixels;//最大可以显示的区域，他不包含通知栏
             height -= visibleFrame.bottom;//anchor 底部坐标位置，这里他剪掉了通知栏高度，使用下面要把通知栏高度加上，不然底部会显示一个空白
-            height += resources.getDimensionPixelSize(resources.getIdentifier("status_bar_height", "dimen", "android"));
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.Q){
+                height += resources.getDimensionPixelSize(resources.getIdentifier("status_bar_height", "dimen", "android"));
+            }
             setHeight(height);
         }
         super.showAsDropDown(anchor, xoff, yoff);
